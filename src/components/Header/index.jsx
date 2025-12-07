1b51ed64814cf7059b182f4e58c516293ffe2674
slbob <44273800+slbob@users.noreply.github.com>
2025-12-07 09:21:31 -0800

    Create index.jsx

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header-title">Weather Forecast</h1>
+    </header>
+  );
+}
