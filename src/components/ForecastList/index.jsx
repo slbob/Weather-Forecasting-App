de45df18cd3c89cff1c2a9cde5cc5d0b4837f1e9
slbob <44273800+slbob@users.noreply.github.com>
2025-12-07 10:06:21 -0800

    Update index.jsx

--- src/components/ForecastList/index.jsx
+++ src/components/ForecastList/index.jsx
@@ -1,1 +1,15 @@
-import React from 'react';import ForecastItem from '../ForecastItem';export default function ForecastList({forecast}){if(!forecast)return null;return(<div>{forecast.map((i,k)=><ForecastItem key={k} item={i}/>)}</div>);}
+import React from "react";
+import ForecastItem from "../ForecastItem";
+import "./styles.css";
+
+export default function ForecastList({ forecast }) {
+  if (!forecast?.length) return null;
+
+  return (
+    <div className="forecast-list">
+      {forecast.map((item, i) => (
+        <ForecastItem key={i} item={item} />
+      ))}
+    </div>
+  );
+}
