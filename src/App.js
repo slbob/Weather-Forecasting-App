572cea93c21ab13d7a77b65a2724397f3be188b8
slbob <44273800+slbob@users.noreply.github.com>
2025-12-07 10:08:50 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,4 @@
-import React from 'react';import Home from './pages/Home';export default function App(){return <Home/>;}
+import React from 'react'
+  import Home from './pages/Home'
+    export default function App()
+    {return <Home/>;}
