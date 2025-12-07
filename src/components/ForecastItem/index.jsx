003026f22b541b8545f4a378ee0d17ad715189e1
slbob <44273800+slbob@users.noreply.github.com>
2025-12-07 10:07:01 -0800

    Update index.jsx

--- src/components/ForecastItem/index.jsx
+++ src/components/ForecastItem/index.jsx
@@ -1,1 +1,12 @@
-import React from 'react';export default function ForecastItem({item}){ return(<div><p>{item.day}</p><p>{item.temp}°C</p></div>);}
+import React from "react";
+import "./styles.css";
+
+export default function ForecastItem({ item }) {
+  return (
+    <div className="forecast-item">
+      <p className="day">{item.day}</p>
+      <p className="temp">{Math.round(item.temp)}°C</p>
+      <p className="desc">{item.description}</p>
+    </div>
+  );
+}
