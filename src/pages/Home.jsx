301db795e061546a87a9a3bf999cd7c46fa46f64
slbob <44273800+slbob@users.noreply.github.com>
2025-12-07 10:20:45 -0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,34 @@
-import React from 'react';import Header from '../components/Header';import SearchBar from '../components/SearchBar';import WeatherCard from '../components/WeatherCard';import ForecastList from '../components/ForecastList';import ErrorBanner from '../components/ErrorBanner';import Loader from '../components/Loader';import useWeather from '../hooks/useWeather';export default function Home(){const {loading,error,weather,forecast,searchCity}=useWeather();return (<div><Header/><SearchBar onSearch={searchCity}/>{error && <ErrorBanner message={error}/>} {loading && <Loader/>}<WeatherCard weather={weather}/> <ForecastList forecast={forecast}/></div>);}
+import React from "react";
+import Header from "../components/Header";
+import SearchBar from "../components/SearchBar";
+import WeatherCard from "../components/WeatherCard";
+import ForecastList from "../components/ForecastList";
+import ErrorBanner from "../components/ErrorBanner";
+import Loader from "../components/Loader";
+import useWeather from "../hooks/useWeather";
+import "../styles/global.css";
+
+export default function Home() {
+  const { weather, forecast, loading, error, searchCity } = useWeather();
+
+  return (
+    <div className="home-page">
+      <Header />
+      
+      {/* Search input */}
+      <SearchBar onSearch={searchCity} />
+
+      {/* Error message */}
+      {error && <ErrorBanner message={error} />}
+
+      {/* Loading spinner */}
+      {loading && <Loader />}
+
+      {/* Current weather */}
+      {weather && <WeatherCard weather={weather} />}
+
+      {/* 5-day forecast */}
+      {forecast && forecast.length > 0 && <ForecastList forecast={forecast} />}
+    </div>
+  );
+}
